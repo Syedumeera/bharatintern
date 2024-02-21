b9a2d69871b93bd7def1568407eb19baeac1432d
Syedumeera <159609461+Syedumeera@users.noreply.github.com>
2024-02-21 20:59:32 +0530

    Update registration.js

--- registration.js
+++ registration.js
@@ -17,3 +17,38 @@ const userSchema = new Schema({
   email: String,
   password: String
 });
+
+const User = model('User', userSchema);
+
+// Middleware to parse JSON
+app.use(json());
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/registration.html');
+});
+
+// Handle form submissions
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Create a new user
+  const newUser = new User({
+    username,
+    email,
+    password
+  });
+
+  try {
+    // Save the user to the database
+    await newUser.save();
+    res.send('Registration successful!');
+  } catch (error) {
+    res.status(500).send('Error registering user.');
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
